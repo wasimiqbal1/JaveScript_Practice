03f4a8e081d827aecf47f77e4deb7fb3e133a86b
wasimiqbal1 <wasim_iqbal@msn.com>
2021-05-16 22:29:21 +0500

    First Commit

---
+++ app.js
@@ -0,0 +1,18 @@
+//Show an alert with an external script
+
+Code
+alert('Hello, world!');
+
+
+/* Code structure
+The first thing we’ll study is the building blocks of code.
+Statements
+Statements are syntax constructs and commands that perform actions.
+We’ve already seen a statement, alert('Hello, world!'), which shows the message “Hello, world!”.
+We can have as many statements in our code as we want. Statements can be separated with a semicolon.
+
+For example, here we split “Hello World” into two alerts:*/
+
+Code
+alert('Hello');
+alert('World');
