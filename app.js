99987a605028173a241cd83a3d42962e23d4fedd
wasimiqbal1 <wasim_iqbal@msn.com>
2021-05-16 23:04:05 +0500

    Update app.js

--- app.js
+++ app.js
@@ -16,3 +16,37 @@ For example, here we split “Hello World” into two alerts:*/
 Code
 alert('Hello');
 alert('World');
+
+// Variables
+// A variable is a “named storage” for data. We can use variables to store goodies, visitors, and other data.
+// To create a variable in JavaScript, use the let or var keyword.
+
+let message;
+message = 'Hello'; // store the string
+alert(message); // shows the variable content
+
+// We can also declare multiple variables in one line:
+
+let user = 'John',
+    age = 25,
+    message = 'Hello';
+
+// var instead of let
+// In older scripts, you may also find another keyword: var instead of let:
+
+// Constants
+// To declare a constant (unchanging) variable, use const instead of let:
+
+const myBirthday = '01.11.1980';
+myBirthday = '01.01.2001'; // error, can't reassign the constant!
+
+// When a programmer is sure that a variable will never change, they can declare it with const to guarantee and clearly communicate that fact to everyone.
+
+// We can declare variables to store data by using the var, let, or const keywords.
+
+// let – is a modern variable declaration.
+// var – is an old-school variable declaration. Normally we don’t use it at all, but we’ll cover subtle differences from let in the chapter The old "var", just in case you need them.
+// const – is like let, but the value of the variable can’t be changed.
+// Variables should be named in a way that allows us to easily understand what’s inside them.
+
+// Data types
