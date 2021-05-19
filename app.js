975906da224b8aa9a90575d91c343ea7c32afbbd
wasimiqbal1 <wasim_iqbal@msn.com>
2021-05-19 23:44:21 +0500

    Type Conversions

--- app.js
+++ app.js
@@ -174,3 +174,52 @@ alert(`You are ${age} years old!`); // You are 25 years old!
 let isBoss = confirm("Are you the boss?");
 
 alert(isBoss); // true if OK is pressed
+
+// Type Conversions
+// Most of the time, operators and functions automatically convert the values given to them to the right type.
+
+// String Conversion
+
+// String conversion happens when we need the string form of a value.
+// For example, alert(value) does it to show the value.
+// We can also call the String(value) function to convert a value to a string:
+
+let value = true;
+alert(typeof value); // boolean
+
+value = String(value); // now value is a string "true"
+alert(typeof value); // string
+
+// Numeric Conversion
+// Numeric conversion happens in mathematical functions and expressions automatically.
+
+alert("6" / "2"); // 3, strings are converted to numbers
+
+// We can use the Number(value) function to explicitly convert a value to a number:
+
+let str = "123";
+alert(typeof str); // string
+
+let num = Number(str); // becomes a number 123
+
+alert(typeof num); // number
+
+// Explicit conversion is usually required when we read a value from a string-based source like a text form but expect a number to be entered.
+
+let age = Number("an arbitrary string instead of a number");
+
+alert(age); // NaN, conversion failed
+
+
+// Boolean Conversion
+// Boolean conversion is the simplest one.
+// It happens in logical operations (later we’ll meet condition tests and other similar things) but can also be performed explicitly with a call to Boolean(value).
+// The conversion rule:
+// Values that are intuitively “empty”, like 0, an empty string, null, undefined, and NaN, become false.
+// Other values become true.
+
+alert(Boolean(1)); // true
+alert(Boolean(0)); // false
+
+alert(Boolean("hello")); // true
+alert(Boolean("")); // false
