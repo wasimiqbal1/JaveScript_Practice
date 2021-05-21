f3a450901fd308c2e84b857be5462bd2e32f3128
wasimiqbal1 <wasim_iqbal@msn.com>
2021-05-22 00:16:02 +0500

    loops summary

--- app.js
+++ app.js
@@ -556,3 +556,16 @@ for (let i = 0; i < 3; i++) {
 }
 
 alert('Done!');
+
+
+// Summary
+// We covered 3 types of loops:
+
+// while – The condition is checked before each iteration.
+// do..while – The condition is checked after each iteration.
+// for (;;) – The condition is checked before each iteration, additional settings available.
+// To make an “infinite” loop, usually the while(true) construct is used. Such a loop, just like any other, can be stopped with the break directive.
+
+// If we don’t want to do anything in the current iteration and would like to forward to the next one, we can use the continue directive.
+
+// break/continue support labels before the loop. A label is the only way for break/continue to escape a nested loop to go to an outer one
