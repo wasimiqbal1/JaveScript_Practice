c6b82fefd199ee4d79c9a236bae3a1180b721216
wasimiqbal1 <wasim_iqbal@msn.com>
2021-05-21 23:59:26 +0500

    for loop

--- app.js
+++ app.js
@@ -513,3 +513,30 @@ while (i) { // when i becomes 0, the condition becomes falsy, and the loop stops
     alert(i);
     i--;
 }
+
+// The “do…while” loop
+// The condition check can be moved below the loop body using the do..while syntax:
+do {
+    // loop body
+} while (condition);
+
+// The loop will first execute the body, then check the condition, and, while it’s truthy, execute it again and again.
+
+let i = 0;
+do {
+    alert(i);
+    i++;
+} while (i < 3);
+
+// This form of syntax should only be used when you want the body of the loop to execute at least once regardless of the condition being truthy. Usually, the other form is preferred: while(…) {…}.
+
+// The “for” loop
+// The for loop is more complex, but it’s also the most commonly used loop.
+
+for (begin; condition; step) {
+    // ... loop body ...
+}
+
+for (let i = 0; i < 3; i++) { // shows 0, then 1, then 2
+    alert(i);
+}
