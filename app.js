9e84a7bece3456327050b31005981de40a45eece
wasimiqbal1 <wasim_iqbal@msn.com>
2021-05-23 23:39:11 +0500

    switch statment

--- app.js
+++ app.js
@@ -569,3 +569,50 @@ alert('Done!');
 // If we don’t want to do anything in the current iteration and would like to forward to the next one, we can use the continue directive.
 
 // break/continue support labels before the loop. A label is the only way for break/continue to escape a nested loop to go to an outer one
+
+
+// The "switch" statement
+// A switch statement can replace multiple if checks.
+
+// It looks like this:
+
+switch (x) {
+    case 'value1': // if (x === 'value1')
+        ...
+        [
+            break
+        ]
+
+    case 'value2': // if (x === 'value2')
+        ...
+        [
+            break
+        ]
+
+    default:
+        ...
+        [
+            break
+        ]
+}
+
+
+//   The value of x is checked for a strict equality to the value from the first case (that is, value1) then to the second (value2) and so on.
+// If the equality is found, switch starts to execute the code starting from the corresponding case, until the nearest break (or until the end of switch).
+// If no case is matched then the default code is executed (if it exists)
+
+let a = 2 + 2;
+
+switch (a) {
+    case 3:
+        alert('Too small');
+        break;
+    case 4:
+        alert('Exactly!');
+        break;
+    case 5:
+        alert('Too big');
+        break;
+    default:
+        alert("I don't know such values");
+}
