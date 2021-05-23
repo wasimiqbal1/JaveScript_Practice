ad53d7aa79f912092076e1f3eed126785592caaa
wasimiqbal1 <wasim_iqbal@msn.com>
2021-05-23 23:42:07 +0500

    switch statment

--- app.js
+++ app.js
@@ -616,3 +616,18 @@ switch (a) {
     default:
         alert("I don't know such values");
 }
+
+// An example without break:
+
+let a = 2 + 2;
+
+switch (a) {
+    case 3:
+        alert('Too small');
+    case 4:
+        alert('Exactly!');
+    case 5:
+        alert('Too big');
+    default:
+        alert("I don't know such values");
+}
