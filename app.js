038aee28f1aa689a99fc8fd044e21cba64636936
wasimiqbal1 <wasim_iqbal@msn.com>
2021-08-22 15:06:32 +0500

    arrow func

--- app.js
+++ app.js
@@ -1063,3 +1063,32 @@ let sum = function(a, b) {
 */
 
 alert(sum(1, 2)); // 3
+
+
+// Replace Function Expressions with arrow functions in the code below:
+
+// NORMAL CODE
+
+function ask(question, yes, no) {
+    if (confirm(question)) yes();
+    else no();
+}
+
+ask(
+    "Do you agree?",
+    function() { alert("You agreed."); },
+    function() { alert("You canceled the execution."); }
+);
+
+//   Looks short and clean,
+
+function ask(question, yes, no) {
+    if (confirm(question)) yes();
+    else no();
+}
+
+ask(
+    "Do you agree?",
+    () => alert("You agreed."),
+    () => alert("You canceled the execution.")
+);
