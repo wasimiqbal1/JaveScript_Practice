b0eaaf2847978e812b5b8900b568b44a03c3a837
wasimiqbal1 <wasim_iqbal@msn.com>
2021-08-22 14:58:07 +0500

    arrow func

--- app.js
+++ app.js
@@ -1036,3 +1036,30 @@ let sum = function(a, b) {
 
 // Arrow functions, the basics
 // There’s another very simple and concise syntax for creating functions, that’s often better than Function Expressions.
+
+// Arrow functions, the basics
+// There’s another very simple and concise syntax for creating functions, that’s often better than Function Expressions.
+// It’s called “arrow functions”, because it looks like this:
+
+let func = (arg1, arg2, ..., argN) => expression
+
+// …This creates a function func that accepts arguments arg1..argN, then evaluates the expression on the right side with their use and returns its result.
+
+// In other words, it’s the shorter version of:
+
+let func = function(arg1, arg2, ..., argN) {
+    return expression;
+};
+
+//   Let’s see a concrete example:
+
+let sum = (a, b) => a + b;
+
+/* This arrow function is a shorter form of:
+
+let sum = function(a, b) {
+  return a + b;
+};
+*/
+
+alert(sum(1, 2)); // 3
